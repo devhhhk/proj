e42e8f7eec4194bd2b87f514838cf0d02df1b956
devhhhk <hhhk0815@gmail.com>
2023-11-07 01:13:44 +0900

    섹션4 수강 완료

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import React from 'react';
-//import './App.css';
+import React, {useState} from 'react';
+import './App.css';
 import Header from './header';
+import Counter from './counter';
+import Container from './Container';
 
 function App() {
-  let name = "GDSC";
-
-  const style = {
-    App: {
-      backgroundColor: 'black',
-    },
-    h1: {
-      color: 'red',
-    },
-    bold_text: {
-      color: 'green',
-    },
+  const counterProps = {
+    initialValue: 5
   }
 
-  const number = 5;
-
   return (
-    <div className="App" style={style.App}>
-      <Header></Header>
-      <h1 style={style.h1}>Hi, React! Bye, JavaScript!</h1>
-      <b style={style.bold_text} id='bold_text'>
-        {number}는 {(number % 2 === 0) ? '짝수': '홀수'}입니다.
-      </b>
-    </div>
+    <Container>
+      <div className="App">
+        <Header></Header>
+        <Counter {...counterProps}></Counter>
+      </div>
+    </Container>
   );
 }
 

0108fcd7b5e93d03632adbca2df330ac544eaa0e
devhhhk <hhhk0815@gmail.com>
2023-11-06 18:46:42 +0900

    커밋 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './App.css';
+import Header from './header';
 
 function App() {
+  let name = "GDSC";
+
+  const style = {
+    App: {
+      backgroundColor: 'black',
+    },
+    h1: {
+      color: 'red',
+    },
+    bold_text: {
+      color: 'green',
+    },
+  }
+
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style.App}>
+      <Header></Header>
+      <h1 style={style.h1}>Hi, React! Bye, JavaScript!</h1>
+      <b style={style.bold_text} id='bold_text'>
+        {number}는 {(number % 2 === 0) ? '짝수': '홀수'}입니다.
+      </b>
     </div>
   );
 }
